00ffe5e7c3ff24a0649837ce94f4aeb348f84a63
Rahmani Said <rahmanaboussaidy09@gmail.com>
2025-09-01 10:15:22 +0300

    descriptions

--- demo.js
+++ demo.js
@@ -1,3 +1,10 @@
-console.log("Hellow World")
-console.error("Error")
-console.warn("Warning")
+let firstName = "John";
+let lastName = "samwel";
+let fullName = firstName +  lastName;
+console.log(fullName)
+
+let str1 = "Hello";
+let str2 = "world";
+
+let result = str1.concat(" ", str2);
+console.log(result)
