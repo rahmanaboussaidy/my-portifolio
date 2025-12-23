d63c0fce02fe0261f9ce62d873dbb4cfc75f3e22
Rahmani Said <rahmanaboussaidy09@gmail.com>
2025-12-23 10:01:12 +0300

    First Html/Css/Js project

---
+++ project/script.js
@@ -0,0 +1,5 @@
+// This is your external JavaScript file
+console.log("The JavaScript file is linked and running!");
+
+// You can add more JavaScript code here
+alert("Hello from the external script!");
